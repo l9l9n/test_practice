d55b74ee463b9b9d87c95459efd7542eb5fcb5df
daniyar342 <tolomushev33@gmail.com>
2023-11-26 01:00:50 +0600

    new__surname

--- products/models.py
+++ products/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Product(models.Model):
     name2 = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=7,decimal_places=2)
