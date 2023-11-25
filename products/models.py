0cb37f96888e2d464e01126e456787cec8fadde7
daniyar342 <tolomushev33@gmail.com>
2023-11-26 01:26:33 +0600

    node1234_commmited in models

--- products/models.py
+++ products/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     mode = models.CharField(verbose_name='Режим')
+    mode2 = models.CharField(verbose_name='Режим')
+    mode3 = models.CharField(verbose_name='Режим')
+    mode4 = models.CharField(verbose_name='Режим')
     # change mode
