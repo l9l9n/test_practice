2caf63f73262d1f96a7e01a64a95826a6f6c34a8
daniyar342 <tolomushev33@gmail.com>
2023-11-26 00:41:07 +0600

    firstcommit_doni

--- products/models.py
+++ products/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name2 = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=7,decimal_places=2)
