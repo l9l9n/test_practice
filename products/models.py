f6d1cb8aee3407c4fd4d13bd829a8c2b94bc2586
l9L9n_PC_Ubuntu <l9l9ndos@gmail.com>
2023-11-26 01:06:10 +0600

    Изменения есть?

--- products/models.py
+++ products/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Product(models.Model):
     name2 = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=7,decimal_places=2)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
