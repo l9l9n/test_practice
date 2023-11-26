efcc7940376111b0fb92e499a3ef1b47e97823c9
daniyar342 <tolomushev33@gmail.com>
2023-11-26 20:15:35 +0600

    node1234_commmited in models12

--- products/models.py
+++ products/models.py
@@ -5,4 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     mode = models.CharField(verbose_name='Режим')
     mode1 = models.CharField(verbose_name='Режим')
+    mode2 = models.CharField(verbose_name='Режим')
+    mode3 = models.CharField(verbose_name='Режим')
     # change mode
